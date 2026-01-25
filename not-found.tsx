f35e51fed09b1f138e5b8f0b55a247c8736b886b
khushi-jaiswal902 <khushi377689@gmail.com>
2026-01-25 21:21:51 +0530

    Add files via upload

---
+++ not-found.tsx
@@ -0,0 +1,8 @@
+export default function notFound() {
+    return (
+        <div>
+            <h1>Page not found</h1>
+            <p>Could not find requested resource</p>
+        </div>
+    )
+}

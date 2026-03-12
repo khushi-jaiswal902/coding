e108c3f3e54611b1afca18cfe4706bc119b14216
khushi-jaiswal902 <khushi377689@gmail.com>
2026-03-12 21:42:07 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,27 +1,16 @@
-import Image from "next/image";
 import Link from "next/link";
-import ComponentPage from "../_components/page";
 
 export const metadata = {
-  title: 'Home'
+    title: 'about'
 }
 
-export default async function Home({searchParams, params}) {
-  console.log(await searchParams);
-  console.log(await params);
-  return (
-    <>
-    <h1>Welcome to my home.</h1>
-    <ComponentPage />
-    <p>
-    <Link href='/about'>About</Link>
-    </p>
-    <p>
-    <Link href='/services'>service</Link>
-    </p>
-    <p>
-    <Link href='/files'>Files</Link>
-    </p>
-    </>
-  );
+export default function About() {
+    return (
+        <>
+        <h1>About Page</h1>
+        <Link href='/'>Home</Link>
+        </>
+    )
 }
+//localhost:3000/about
+
